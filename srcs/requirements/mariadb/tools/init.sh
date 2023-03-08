f702d970fedfb0c9a6585cd12f2421d8e2af1e76
RidaEn-nasry <rida.ennasry@gmail.com>
2023-03-08 14:55:21 +0100

    no much progress

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,23 +6,37 @@
 
 # checking if the db folder exist
 
-# check if the db folder exist
-# if [ ! -d /var/lib/mysql/$MYSQL_NAME ]; then
-    # if yes
-    # echo "data base doesn't exist"
-    # service mysql start
-    # echo "db started"
-    # mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_NAME;\
-    # ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';\
-    # GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';\
-    # FLUSH PRIVILEGES;"
-    # kill mysql process
-    # killall mqld
-# fi
+/etc/init.d/mysql start \
+    && mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+                          CREATE DATABASE IF NOT EXISTS $MYSQL_NAME;\
+                          GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%'; \
+                          ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
+                          FLUSH PRIVILEGES;" && /etc/init.d/mysql stop && echo "$@"
 
-/etc/init.d/mysql start && mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_NAME;\
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';\
-GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';\
-FLUSH PRIVILEGES;" &&  service mysql stop
+# mysqld_safe 
 
-exec "$@"
+    # && kill $(ps aux | grep "/usr/sbin/mysqld" | awk '{print $2}' | head -n 1) \
+
+
+# exec "$@" 
+
+
+
+# kill $(ps aux | grep "/usr/sbin/mysqld" | awk '{print $2}' | head -n 1) && service mysql stop
+
+
+#sleep 3 && kill $(ps aux | grep "/usr/sbin/mysqld" | awk '{print $2}' | head -n 1) && sleep 3 
+
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
+# kill indeed kill the process, get it's pid and kill it first line only 
+
+# kill $(ps aux | grep mysqld | awk '{print $2}')
+
+# run the fourth line alone , you know with root -p 
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_NAME;\
+# GRANT ALL PRIVILEGES ON $MYSQL_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';\
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';\
+# FLUSH PRIVILEGES;" && service mysql stop
+
+# mysqld_safe
+

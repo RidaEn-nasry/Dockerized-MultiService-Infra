35d0677d1150de56d775f8132b6d2f9d0b9e4a5d
Rida En-nasry <ren-nasr@e2r4p2.1337.ma>
2023-03-15 10:34:32 +0100

    changing theme

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -23,7 +23,7 @@ sleep 30
 wp core install --url=$WP_URL --title=$WP_SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD --allow-root
 
-wp theme install artsylens --activate --allow-root
+wp theme install twentytwentytwo --activate --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
